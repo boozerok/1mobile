1fe16e829b2cca533852ad3ee2177e5a55048419
Evgeni Ivanov <e_ivanov@corp.mail.ru>
2013-04-15 23:05:03 +0400

    handlebars

---
+++ data/content/json/races.js
@@ -0,0 +1,118 @@
+var racesData = {
+	"races" : [
+		{
+			"race_id"			: "3",
+			"race_alias" 		: "china",
+			"race_name" 		: "Гран При Китая",
+			"race_timetable" 	: {
+				"practice_1" 	: "2013-03-22T10:00/P1H30M",
+				"practice_2" 	: "2013-03-22T14:00/P1H30M",
+				"practice_3" 	: "2013-03-23T13:00/P1H30M",
+				"qualifying" 	: "2013-03-24T16:00",
+				"race" 			: "2013-03-25T16:00"
+			},
+			"race_circuit" 			: {
+				"curcuit_name" 		: "Сепанг",
+				"curcuit_laps" 		: "56",
+				"curcuit_length" 	: "5.543"
+			},
+			"race_distance" 	: "310.408",
+			"race_pics" : [
+				{
+					"src" 	: "http://kereta.info/wp-content/uploads/2009/03/f1-malaysia-22.jpg",
+					"copy" 	: "http://kereta.info/watch-jamiroquai-concert-at-the-petronas-malaysian-grandprix-2009-with-only-the-f1-ticket/"
+				},
+				{
+					"src" 	: "http://www.f1fanatic.co.uk/wp-content/uploads/2010/04/malaysian_gp_start_sepa_2010-5.jpg",
+					"copy" 	: ""
+				},
+				{
+					"src" 	: "http://mrdefinite.com/wp-content/uploads/2011/04/petronas-malaysia-grand-prix-motorsports-F1-models.jpg",
+					"copy" 	: "http://im.mrdefinite.com/mr-definite-as-petronas-f1-driver/",
+					"text" 	: "Модели на ГП Малайзии"
+				}
+			],
+			"social" : {
+				"twitter" 		: "",
+				"instagram" 	: ""
+			}
+		},
+		{
+			"race_id"			: "2",
+			"race_alias" 		: "malaysia",
+			"race_name" 		: "Гран При Малайзии",
+			"race_timetable" 	: {
+				"practice_1" 	: "2013-03-22T10:00/P1H30M",
+				"practice_2" 	: "2013-03-22T14:00/P1H30M",
+				"practice_3" 	: "2013-03-23T13:00/P1H30M",
+				"qualifying" 	: "2013-03-24T16:00",
+				"race" 			: "2013-03-25T16:00"
+			},
+			"race_circuit" 			: {
+				"curcuit_name" 		: "Сепанг",
+				"curcuit_laps" 		: "56",
+				"curcuit_length" 	: "5.543"
+			},
+			"race_distance" 	: "310.408",
+			"race_pics" : [
+				{
+					"src" 	: "http://kereta.info/wp-content/uploads/2009/03/f1-malaysia-22.jpg",
+					"copy" 	: "http://kereta.info/watch-jamiroquai-concert-at-the-petronas-malaysian-grandprix-2009-with-only-the-f1-ticket/"
+				},
+				{
+					"src" 	: "http://www.f1fanatic.co.uk/wp-content/uploads/2010/04/malaysian_gp_start_sepa_2010-5.jpg",
+					"copy" 	: ""
+				},
+				{
+					"src" 	: "http://mrdefinite.com/wp-content/uploads/2011/04/petronas-malaysia-grand-prix-motorsports-F1-models.jpg",
+					"copy" 	: "http://im.mrdefinite.com/mr-definite-as-petronas-f1-driver/",
+					"text" 	: "Модели на ГП Малайзии"
+				}
+			],
+			"social" : {
+				"twitter" 		: "",
+				"instagram" 	: ""
+			}
+		},
+		{
+			"race_id"			: "1",
+			"race_alias" 		: "australia",
+			"race_name" 		: "Гран При Австралии",
+			"race_timetable" 	: {
+				"practice_1" 	: "2013-03-15T12:30/P1H30M",
+				"practice_2" 	: "2013-03-15T16:30/P1H30M",
+				"practice_3" 	: "2013-03-15T14:00/P1H30M",
+				"qualifying" 	: "2013-03-16T17:00",
+				"race" 			: "2013-03-17T17:00"
+			},
+			"race_circuit" 			: {
+				"curcuit_name" 		: "Альберт Парк",
+				"curcuit_laps" 		: "58",
+				"curcuit_length" 	: "5.303"
+			},
+			"race_distance" 	: "307.574",
+			"race_pics" : [
+				{
+					"src" 	: "http://images.planetf1.com/12/02/800x600/2011-Australian-Grand-Prix_2718929.jpg",
+					"copy" 	: "http://www.planet-f1.com/driver/3265/7593952/Australian-Grand-Prix-Preview"
+				},
+				{
+					"src" 	: "http://vladogatev.com/wp-content/uploads/2013/03/australia-starting-grid.jpg",
+					"copy" 	: ""
+				},
+				{
+					"src" 	: "http://mercedesbenzblogphotodb.files.wordpress.com/2012/03/australian-f1-gp-2012-preview-mbamgf1.jpg",
+					"copy" 	: ""
+				},
+				{
+					"src" 	: "http://cdn.theweek.co.uk/sites/theweek/files/styles/theweek_gallery_main_image/public/120316nipformula2.jpg",
+					"copy" 	: ""
+				}
+			],
+			"social" : {
+				"twitter" 	: "ausgrandprix",
+				"youtube" 	: "australiangrandprix"
+			}
+		}
+	]
+};
