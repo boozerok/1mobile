38dfd62d6afe301b42dd34c5a8693766fdc60d67
Evgeni Ivanov <geno.ivanov@gmail.com>
2013-04-30 03:05:11 +0400

    new js-logic w/ tempo

--- data/content/json/races.js
+++ data/content/json/races.js
@@ -1,61 +1,65 @@
 var racesData = {
 	"races" : [
+
 		{
-			"race_id"			: "5",
-			"race_alias" 		: "barcelona",
-			"race_name" 		: "Gran Premio De Espana",
-			"race_country" 		: "spain",
+			"race_id"			: "1",
+			"race_alias" 		: "australia",
+			"race_name" 		: "Rolex Australian Grand Prix",
+			"race_country" 		: "australia",
 			"race_timetable" 	: {
-				"practice_1" 	: "2013-05-10T10:00:00",
-				"practice_2" 	: "2013-05-10T14:00:00",
-				"practice_3" 	: "2013-05-11T11:00:00",
-				"qualifying" 	: "2013-05-11T14:00:00",
-				"race" 			: "2013-05-12T14:00:00"
+				"practice_1" 	: "2013-03-15T12:30:00",
+				"practice_2" 	: "2013-03-15T16:30:00",
+				"practice_3" 	: "2013-03-15T14:00:00",
+				"qualifying" 	: "2013-03-16T17:00:00",
+				"race" 			: "2013-03-17T17:00:00"
 			},
 			"race_circuit" 			: {
-				"curcuit_name" 		: "Circuit de Catalunya",
-				"curcuit_laps" 		: "66",
-				"curcuit_length" 	: "4.655"
+				"curcuit_name" 		: "Albert Park",
+				"curcuit_laps" 		: "58",
+				"curcuit_length" 	: "5.303"
 			},
-			"race_distance" 	: "307.104",
+			"race_distance" 	: "307.574",
 			"race_pics" : [
 				{
-					"src" 	: "http://kereta.info/wp-content/uploads/2009/03/f1-malaysia-22.jpg",
-					"copy" 	: "http://kereta.info/watch-jamiroquai-concert-at-the-petronas-malaysian-grandprix-2009-with-only-the-f1-ticket/"
+					"src" 	: "http://images.planetf1.com/12/02/800x600/2011-Australian-Grand-Prix_2718929.jpg",
+					"copy" 	: "http://www.planet-f1.com/driver/3265/7593952/Australian-Grand-Prix-Preview"
 				},
 				{
-					"src" 	: "http://www.f1fanatic.co.uk/wp-content/uploads/2010/04/malaysian_gp_start_sepa_2010-5.jpg",
+					"src" 	: "http://vladogatev.com/wp-content/uploads/2013/03/australia-starting-grid.jpg",
 					"copy" 	: ""
 				},
 				{
-					"src" 	: "http://mrdefinite.com/wp-content/uploads/2011/04/petronas-malaysia-grand-prix-motorsports-F1-models.jpg",
-					"copy" 	: "http://im.mrdefinite.com/mr-definite-as-petronas-f1-driver/",
-					"text" 	: "Модели на ГП Малайзии"
+					"src" 	: "http://mercedesbenzblogphotodb.files.wordpress.com/2012/03/australian-f1-gp-2012-preview-mbamgf1.jpg",
+					"copy" 	: ""
+				},
+				{
+					"src" 	: "http://cdn.theweek.co.uk/sites/theweek/files/styles/theweek_gallery_main_image/public/120316nipformula2.jpg",
+					"copy" 	: ""
 				}
 			],
 			"social" : {
-				"twitter" 		: "",
-				"instagram" 	: ""
+				"twitter" 	: "ausgrandprix",
+				"youtube" 	: "australiangrandprix"
 			}
 		},
 		{
-			"race_id"			: "4",
-			"race_alias" 		: "bahrain",
-			"race_name" 		: "Gulf Air Bahrain Grand Prix",
-			"race_country" 		: "bahrain",
+			"race_id"			: "2",
+			"race_alias" 		: "malaysia",
+			"race_name" 		: "Petronas Malaysia Grand Prix",
+			"race_country" 		: "malaysia",
 			"race_timetable" 	: {
-				"practice_1" 	: "2013-04-19T10:00:00",
-				"practice_2" 	: "2013-04-19T14:00:00",
-				"practice_3" 	: "2013-04-20T11:00:00",
-				"qualifying" 	: "2013-04-20T15:00:00",
-				"race" 			: "2013-04-21T16:00:00"
+				"practice_1" 	: "2013-03-22T10:00:00",
+				"practice_2" 	: "2013-03-22T14:00:00",
+				"practice_3" 	: "2013-03-23T13:00:00",
+				"qualifying" 	: "2013-03-24T16:00:00",
+				"race" 			: "2013-03-25T16:00:00"
 			},
 			"race_circuit" 			: {
-				"curcuit_name" 		: "Bahrain International Circuit",
-				"curcuit_laps" 		: "57",
-				"curcuit_length" 	: "5.412"
+				"curcuit_name" 		: "Sepang International Circuit",
+				"curcuit_laps" 		: "56",
+				"curcuit_length" 	: "5.543"
 			},
-			"race_distance" 	: "308.238",
+			"race_distance" 	: "310.408",
 			"race_pics" : [
 				{
 					"src" 	: "http://kereta.info/wp-content/uploads/2009/03/f1-malaysia-22.jpg",
@@ -115,23 +119,23 @@ var racesData = {
 			}
 		},
 		{
-			"race_id"			: "2",
-			"race_alias" 		: "malaysia",
-			"race_name" 		: "Petronas Malaysia Grand Prix",
-			"race_country" 		: "malaysia",
+			"race_id"			: "4",
+			"race_alias" 		: "bahrain",
+			"race_name" 		: "Gulf Air Bahrain Grand Prix",
+			"race_country" 		: "bahrain",
 			"race_timetable" 	: {
-				"practice_1" 	: "2013-03-22T10:00:00",
-				"practice_2" 	: "2013-03-22T14:00:00",
-				"practice_3" 	: "2013-03-23T13:00:00",
-				"qualifying" 	: "2013-03-24T16:00:00",
-				"race" 			: "2013-03-25T16:00:00"
+				"practice_1" 	: "2013-04-19T10:00:00",
+				"practice_2" 	: "2013-04-19T14:00:00",
+				"practice_3" 	: "2013-04-20T11:00:00",
+				"qualifying" 	: "2013-04-20T15:00:00",
+				"race" 			: "2013-04-21T16:00:00"
 			},
 			"race_circuit" 			: {
-				"curcuit_name" 		: "Sepang International Circuit",
-				"curcuit_laps" 		: "56",
-				"curcuit_length" 	: "5.543"
+				"curcuit_name" 		: "Bahrain International Circuit",
+				"curcuit_laps" 		: "57",
+				"curcuit_length" 	: "5.412"
 			},
-			"race_distance" 	: "310.408",
+			"race_distance" 	: "308.238",
 			"race_pics" : [
 				{
 					"src" 	: "http://kereta.info/wp-content/uploads/2009/03/f1-malaysia-22.jpg",
@@ -153,45 +157,81 @@ var racesData = {
 			}
 		},
 		{
-			"race_id"			: "1",
-			"race_alias" 		: "australia",
-			"race_name" 		: "Rolex Australian Grand Prix",
-			"race_country" 		: "australia",
+			"race_id"			: "5",
+			"race_alias" 		: "barcelona",
+			"race_name" 		: "Gran Premio De Espana",
+			"race_country" 		: "spain",
 			"race_timetable" 	: {
-				"practice_1" 	: "2013-03-15T12:30:00",
-				"practice_2" 	: "2013-03-15T16:30:00",
-				"practice_3" 	: "2013-03-15T14:00:00",
-				"qualifying" 	: "2013-03-16T17:00:00",
-				"race" 			: "2013-03-17T17:00:00"
+				"practice_1" 	: "2013-05-10T10:00:00",
+				"practice_2" 	: "2013-05-10T14:00:00",
+				"practice_3" 	: "2013-05-11T11:00:00",
+				"qualifying" 	: "2013-05-11T14:00:00",
+				"race" 			: "2013-05-12T14:00:00"
 			},
 			"race_circuit" 			: {
-				"curcuit_name" 		: "Albert Park",
-				"curcuit_laps" 		: "58",
-				"curcuit_length" 	: "5.303"
+				"curcuit_name" 		: "Circuit de Catalunya",
+				"curcuit_laps" 		: "66",
+				"curcuit_length" 	: "4.655"
 			},
-			"race_distance" 	: "307.574",
+			"race_distance" 	: "307.104",
 			"race_pics" : [
 				{
-					"src" 	: "http://images.planetf1.com/12/02/800x600/2011-Australian-Grand-Prix_2718929.jpg",
-					"copy" 	: "http://www.planet-f1.com/driver/3265/7593952/Australian-Grand-Prix-Preview"
+					"src" 	: "http://kereta.info/wp-content/uploads/2009/03/f1-malaysia-22.jpg",
+					"copy" 	: "http://kereta.info/watch-jamiroquai-concert-at-the-petronas-malaysian-grandprix-2009-with-only-the-f1-ticket/"
 				},
 				{
-					"src" 	: "http://vladogatev.com/wp-content/uploads/2013/03/australia-starting-grid.jpg",
+					"src" 	: "http://www.f1fanatic.co.uk/wp-content/uploads/2010/04/malaysian_gp_start_sepa_2010-5.jpg",
 					"copy" 	: ""
 				},
 				{
-					"src" 	: "http://mercedesbenzblogphotodb.files.wordpress.com/2012/03/australian-f1-gp-2012-preview-mbamgf1.jpg",
-					"copy" 	: ""
-				},
-				{
-					"src" 	: "http://cdn.theweek.co.uk/sites/theweek/files/styles/theweek_gallery_main_image/public/120316nipformula2.jpg",
-					"copy" 	: ""
+					"src" 	: "http://mrdefinite.com/wp-content/uploads/2011/04/petronas-malaysia-grand-prix-motorsports-F1-models.jpg",
+					"copy" 	: "http://im.mrdefinite.com/mr-definite-as-petronas-f1-driver/",
+					"text" 	: "Модели на ГП Малайзии"
 				}
 			],
 			"social" : {
-				"twitter" 	: "ausgrandprix",
-				"youtube" 	: "australiangrandprix"
+				"twitter" 		: "",
+				"instagram" 	: ""
+			}
+		},
+		{
+			"race_id"			: "6",
+			"race_alias" 		: "monaco",
+			"race_name" 		: "Grand Prix De Monaco",
+			"race_country" 		: "monaco",
+			"race_timetable" 	: {
+				"practice_1" 	: "2013-05-23T10:00:00",
+				"practice_2" 	: "2013-05-23T14:00:00",
+				"practice_3" 	: "2013-05-25T11:00:00",
+				"qualifying" 	: "2013-05-25T14:00:00",
+				"race" 			: "2013-05-26T14:00:00"
+			},
+			"race_circuit" 			: {
+				"curcuit_name" 		: "Circuit de Monaco",
+				"curcuit_laps" 		: "78",
+				"curcuit_length" 	: "3.340"
+			},
+			"race_distance" 	: "260.520",
+			"race_pics" : [
+				{
+					"src" 	: "http://kereta.info/wp-content/uploads/2009/03/f1-malaysia-22.jpg",
+					"copy" 	: "http://kereta.info/watch-jamiroquai-concert-at-the-petronas-malaysian-grandprix-2009-with-only-the-f1-ticket/"
+				},
+				{
+					"src" 	: "http://www.f1fanatic.co.uk/wp-content/uploads/2010/04/malaysian_gp_start_sepa_2010-5.jpg",
+					"copy" 	: ""
+				},
+				{
+					"src" 	: "http://mrdefinite.com/wp-content/uploads/2011/04/petronas-malaysia-grand-prix-motorsports-F1-models.jpg",
+					"copy" 	: "http://im.mrdefinite.com/mr-definite-as-petronas-f1-driver/",
+					"text" 	: "Модели на ГП Малайзии"
+				}
+			],
+			"social" : {
+				"twitter" 		: "",
+				"instagram" 	: ""
 			}
 		}
+
 	]
 };
